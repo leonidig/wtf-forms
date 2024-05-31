4b177d9c13411c8f0c631d18fd2690ddd2f72f83
Leonidig <leonid200955@gmail.com>
2024-05-31 11:28:36 +0300

    test flask - wtf

--- app/app.py
+++ app/app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+
+from form import UserForm
+
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.get("/")
 def index():
-    return render_template("index.html")
+    form = UserForm()
+    return render_template("index.html", form = form)
 
 
-if __name__ == "__main__":
+@app.post("/")
+def submit():
+    form = UserForm()
+    if form.validate_on_submit():
+        data = {
+            "name": form.name.data,
+            "email": form.email.data
+        }
+        save_to_json(data)
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form)
+
+
+def save_to_json(data):
+    with open("data.json", "a", encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
+        file.write('\n')
+
+if __name__ == '__main__':
     app.run(debug=True)
